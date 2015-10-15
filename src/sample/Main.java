2d53d0709bc389d41eb4a9d4cc23d9bfc2d82949
Gordon Li <gordonlimusic@gmail.com>
2015-10-15 18:06:52 -0400

    League UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -58,7 +58,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLHomeScreen.fxml")); //main screen is home screen
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
 
